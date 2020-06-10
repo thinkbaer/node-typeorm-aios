d92d3ea69786bc0935f023a9e3bd5aa989a56a17
Robert Kania <cezaryrk@yahoo.de>
2020-06-10 18:28:17 +0200

    refactor: tslint

--- src/integrate.ts
+++ src/integrate.ts
@@ -1,27 +1,27 @@
-import {AiosDriver} from "./driver/aios/AiosDriver";
-import {Connection, Driver, EntityManager, QueryRunner} from "typeorm";
-import {DriverFactory} from "typeorm/driver/DriverFactory";
-import {EntityManagerFactory} from "typeorm/entity-manager/EntityManagerFactory";
-import {AiosEntityManager} from "./driver/aios/AiosEntityManager";
+import {AiosDriver} from './driver/aios/AiosDriver';
+import {Connection, Driver, EntityManager, QueryRunner} from 'typeorm';
+import {DriverFactory} from 'typeorm/driver/DriverFactory';
+import {EntityManagerFactory} from 'typeorm/entity-manager/EntityManagerFactory';
+import {AiosEntityManager} from './driver/aios/AiosEntityManager';
 
 /** ===============================================
  * Override typeorm DriverFactory
  */
 const driverFactoryCreate = DriverFactory.prototype.create;
 DriverFactory.prototype.create = function (connection: Connection): Driver {
-    const type = connection.options.type as string;
-    if(type == 'aios'){
-        return new AiosDriver(connection);
-    }
-  return driverFactoryCreate.call(this,connection);
+  const type = connection.options.type as string;
+  if (type === 'aios') {
+    return new AiosDriver(connection);
+  }
+  return driverFactoryCreate.call(this, connection);
 };
 
 
 const entityManagerFactoryCreate = EntityManagerFactory.prototype.create;
 EntityManagerFactory.prototype.create = function (connection: Connection, queryRunner?: QueryRunner): EntityManager {
-  if(connection.driver instanceof AiosDriver){
-    return new AiosEntityManager(connection,queryRunner);
+  if (connection.driver instanceof AiosDriver) {
+    return new AiosEntityManager(connection, queryRunner);
   }
-  return entityManagerFactoryCreate.call(this,connection);
+  return entityManagerFactoryCreate.call(this, connection);
 };
 
